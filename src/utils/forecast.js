bf3eb827a17afbfdca450fa9bb4838a2503f88f9
Satyaki Bose <satyakibose98@gmail.com>
2020-01-12 18:08:16 +0530

    adding weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
 	const url = 'https://api.darksky.net/forecast/c84ea7c1573ba27e688b9107bf0c0137/' + latitude + ',' + longitude + '?units=si';
 	
-	request({url, json: true}, (error, response) => {
+	request({url, json: true}, (error, { body}) => {
 		if (error) {
 			callback('Unable to connect to the location service!', undefined);
 		}
-		else if (response.body.error) {
+		else if (body.error) {
 			callback('Unable to find location. Plase try another Search!', undefined);
 		}
-		else {
-			callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% of rain.`);
+		else { 
+			callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh}. The low today is ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability}% of rain.`);
 		}
 	})
 }
